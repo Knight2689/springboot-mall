2da4c9f2ad6f7367fbf7a5b32892e0a7d026cf0b
風 <rc560168@gmail.com>
2024-06-11 16:49:10 +0800

    初始化 Spring Boot project

---
+++ src/test/java/com/knight2689/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knight2689.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
